66ca4f266daee013392c8404dbd949547205f112
Atul Garg <atulgarg220404@gmail.com>
2025-03-11 21:45:51 +0530

    exposed headers

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,8 @@ app.use(cors({
     origin: ['https://darwin12.netlify.app', 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
+    exposedHeaders: ['x-auth-token']
 }));
 
 // Configure Express to handle large payloads
